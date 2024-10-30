e223104bcccbc6e9656461e957b8fba6b2d80d93
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-10-31 02:05:03 +0600

    Policy authentication and authorization

---
+++ app/Policies/ServicePolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+final class ServicePolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Service $service): bool
+    {
+        return $user->id === $service->user_id;
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->hasVerifiedEmail();
+    }
+
+    public function update(User $user, Service $service): bool
+    {
+        return $user->id === $service->user_id;
+    }
+    public function delete(User $user, Service $service): bool
+    {
+        return $user->id === $service->user_id;
+    }
+
+}
---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+final class UserPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    public function delete(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    public function restore(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+
+    public function forceDelete(User $user, User $model): bool
+    {
+        return $user->id === $model->id;
+    }
+}
