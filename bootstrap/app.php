2db2724f7ee039d6dcf117cab2311523ce442853
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-11-01 09:50:00 +0600

    api versioning

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SunsetMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
+            'sunset' => SunsetMiddleware::class,
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
--- routes/api/routes.php
+++ routes/api/routes.php
@@ -3,21 +3,32 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::middleware(['auth:sanctum', 'throttle:api'])->group(static function (): void {
 
-    Route::get('/user', static function (Request $request) {
-        return $request->user() ;
-    })->name('user');
+Route::prefix('v1')->as('v1:')->group(static function (): void {
+    Route::get('/', static fn() => response()->json(request()->route()))->middleware(['sunset:'. now()->subDays(3)->format('Y-m-d H:i:s')]);
 
-    Route::prefix('services')->as('services:')->group(base_path(
-        path: 'routes/api/services.php'
-    ))->middleware(['throttle:100,1']);
+    Route::middleware(['auth:sanctum', 'throttle:api'])->group(static function (): void {
 
-    Route::prefix('credentials')->as('credentials:')->group(base_path(
-        path: 'routes/api/credentials.php'
-    ));
+        Route::get('/user', static function (Request $request) {
+            return $request->user() ;
+        })->name('user');
 
-    Route::prefix('checks')->as('checks:')->group(base_path(
-        path: 'routes/api/checks.php'
-    ));
+        Route::prefix('services')->as('services:')->group(base_path(
+            path: 'routes/api/v1/services.php'
+        ))->middleware(['throttle:100,1']);
+
+        Route::prefix('credentials')->as('credentials:')->group(base_path(
+            path: 'routes/api/v1/credentials.php'
+        ));
+
+        Route::prefix('checks')->as('checks:')->group(base_path(
+            path: 'routes/api/v1/checks.php'
+        ));
+    });
+});
+
+Route::prefix('v2')->as('v2:')->group(static function (): void {
+    Route::get('/', static fn() => response()->json(request()->route()));
+
+
 });
