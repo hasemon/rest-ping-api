94ae9816548d19ca72ad67f2465dfeade194f4f1
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-11-12 13:43:26 +0600

    caching request

--- routes/api/v1/services.php
+++ routes/api/v1/services.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Services;
 use Illuminate\Support\Facades\Route;
+use Spatie\ResponseCache\Middlewares\CacheResponse;
 
-Route::get('/', Services\IndexController::class)->name('index');
+
 Route::post('/', Services\StoreController::class)->name('store');
+
+Route::middleware([CacheResponse::class])->group(static function (){
+    Route::get('/', Services\IndexController::class)->name('index');
+    Route::get('{ulid}', Services\ShowController::class)->name('show');
+});
+
 Route::get('{ulid}', Services\ShowController::class)->name('show');
 Route::put('{service}', Services\UpdateController::class)->name('update');
 Route::delete('{service}', Services\DeleteController::class)->name('delete');
